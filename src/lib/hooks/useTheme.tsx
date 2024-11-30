0ae2dfeda77143538bdf617f76ae6ddd8f3f5670
gerismumo <geraldmumo6@gmail.com>
2024-11-30 12:16:02 +0300

    window check in ssr

--- src/lib/hooks/useTheme.tsx
+++ src/lib/hooks/useTheme.tsx
@@ -3,21 +3,30 @@
 import { useEffect, useState } from "react";
 
 export default function useTheme() {
-  const [theme, setTheme] = useState(() =>
-    window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light"
-  );
+  const [theme, setTheme] = useState("light");
 
   useEffect(() => {
-    const root = document.documentElement;
-    root.setAttribute("data-theme", theme);
-    
-    if (theme === "dark") {
-      root.classList.add("dark");
-    } else {
-      root.classList.remove("dark");
+    if (typeof window !== "undefined") {
+      const savedTheme = localStorage.getItem("theme");
+      const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+      
+      setTheme(savedTheme || (prefersDark ? "dark" : "light"));
     }
+  }, []);
 
-    localStorage.setItem("theme", theme);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const root = document.documentElement;
+      root.setAttribute("data-theme", theme);
+
+      if (theme === "dark") {
+        root.classList.add("dark");
+      } else {
+        root.classList.remove("dark");
+      }
+
+      localStorage.setItem("theme", theme);
+    }
   }, [theme]);
 
   const toggleTheme = () => {
